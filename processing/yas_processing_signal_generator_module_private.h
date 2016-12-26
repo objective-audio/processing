262a9ee4562968a7dcd5ccc26901f67453931b98
Yuki Yasoshima <yukiyasos@gmail.com>
2016-12-26 10:27:14 +0900

    receive_signal

--- processing/yas_processing_signal_generator_module_private.h
+++ processing/yas_processing_signal_generator_module_private.h
@@ -19,14 +19,10 @@ struct processing::signal_generator_module::impl_base : module::impl {
 
 template <typename T>
 struct processing::signal_generator_module::impl : impl_base {
-    impl(processing::time_range &&time_range, send_signal_f<T> &&handler)
-        : impl_base(std::move(time_range)), _send_signal_handler(std::move(handler)) {
-    }
-
-    void send_signal(processing::time_range const &time_range, int64_t const ch_idx, T *signal_ptr) {
-        if (_send_signal_handler) {
-            _send_signal_handler(time_range, ch_idx, signal_ptr);
-        }
+    impl(args<T> &&args)
+        : impl_base(std::move(args.time_range)),
+          _send_signal_handler(std::move(args.send_signal_handler)),
+          _receive_signal_handler(std::move(args.receive_signal_handler)) {
     }
 
     virtual void process(stream &stream) override final {
@@ -38,62 +34,88 @@ struct processing::signal_generator_module::impl : impl_base {
 
         auto const &current_time_range = *current_time_range_opt;
 
-        for (auto const &connector_pair : output_connectors()) {
-            auto const &connector_key = connector_pair.first;
-            auto const &connector = connector_pair.second;
-            if (connector_key != output_key) {
-                continue;
-            }
+        if (_receive_signal_handler) {
+            for (auto const &connector_pair : input_connectors()) {
+                auto const &connector_key = connector_pair.first;
+                auto const &connector = connector_pair.second;
 
-            auto const &ch_idx = connector.channel_index;
+                auto const &ch_idx = connector.channel_index;
 
-            if (stream.has_channel(ch_idx)) {
-                auto &channel = stream.channel(ch_idx);
-                processing::time_range combined_time_range = current_time_range;
+                if (stream.has_channel(ch_idx)) {
+                    auto const &channel = stream.channel(ch_idx);
 
-                auto predicate = [&current_time_range, &combined_time_range](auto const &pair) {
-                    if (pair.second.sample_type() == typeid(T) && pair.first.can_combine(current_time_range)) {
-                        combined_time_range = *combined_time_range.combine(pair.first);
-                        return true;
-                    }
-                    return false;
-                };
+                    auto predicate = [](auto const &pair) { return pair.second.sample_type() == typeid(T); };
 
-                auto const &filtered_datas = filter(channel.datas(), predicate);
+                    auto const filtered_datas = filter(channel.datas(), predicate);
 
-                if (filtered_datas.size() > 0) {
-                    std::vector<T> vec(combined_time_range.length);
                     for (auto const &pair : filtered_datas) {
-                        auto const length = pair.first.length;
-                        auto const dst_idx = pair.first.start_frame - combined_time_range.start_frame;
-                        auto *dst_ptr = &vec[dst_idx];
-                        auto const *src_ptr = get_raw<T>(pair.second).data();
-                        memcpy(dst_ptr, src_ptr, length * sizeof(T));
+                        if (auto const time_range_opt = current_time_range.intersect(pair.first)) {
+                            auto const &time_range = *time_range_opt;
+                            auto const *ptr = get_raw<T>(pair.second).data();
+                            auto const idx = time_range.start_frame - current_time_range.start_frame;
+                            _receive_signal_handler(time_range, ch_idx, connector_key, &ptr[idx]);
+                        }
                     }
-
-                    channel.erase_data_if(std::move(predicate));
-
-                    send_signal(current_time_range, ch_idx,
-                                &vec[current_time_range.start_frame - combined_time_range.start_frame]);
-
-                    channel.insert_data(combined_time_range, std::move(vec));
-
-                    return;
                 }
-            } else {
-                stream.insert_channel(ch_idx);
             }
+        }
 
-            std::vector<T> vec(current_time_range.length);
+        if (_send_signal_handler) {
+            for (auto const &connector_pair : output_connectors()) {
+                auto const &connector_key = connector_pair.first;
+                auto const &connector = connector_pair.second;
 
-            send_signal(current_time_range, ch_idx, vec.data());
+                auto const &ch_idx = connector.channel_index;
 
-            auto &channel = stream.channel(ch_idx);
-            channel.insert_data(current_time_range, std::move(vec));
+                if (stream.has_channel(ch_idx)) {
+                    auto &channel = stream.channel(ch_idx);
+                    processing::time_range combined_time_range = current_time_range;
+
+                    auto predicate = [&current_time_range, &combined_time_range](auto const &pair) {
+                        if (pair.second.sample_type() == typeid(T) && pair.first.can_combine(current_time_range)) {
+                            combined_time_range = *combined_time_range.combine(pair.first);
+                            return true;
+                        }
+                        return false;
+                    };
+
+                    auto const filtered_datas = filter(channel.datas(), predicate);
+
+                    if (filtered_datas.size() > 0) {
+                        std::vector<T> vec(combined_time_range.length);
+                        for (auto const &pair : filtered_datas) {
+                            auto const length = pair.first.length;
+                            auto const dst_idx = pair.first.start_frame - combined_time_range.start_frame;
+                            auto *dst_ptr = &vec[dst_idx];
+                            auto const *src_ptr = get_raw<T>(pair.second).data();
+                            memcpy(dst_ptr, src_ptr, length * sizeof(T));
+                        }
+
+                        channel.erase_data_if(std::move(predicate));
+
+                        _send_signal_handler(current_time_range, ch_idx, connector_key,
+                                             &vec[current_time_range.start_frame - combined_time_range.start_frame]);
+
+                        channel.insert_data(combined_time_range, std::move(vec));
+
+                        return;
+                    }
+                } else {
+                    stream.insert_channel(ch_idx);
+                }
+
+                std::vector<T> vec(current_time_range.length);
+
+                _send_signal_handler(current_time_range, ch_idx, connector_key, vec.data());
+
+                auto &channel = stream.channel(ch_idx);
+                channel.insert_data(current_time_range, std::move(vec));
+            }
         }
     }
 
    private:
     send_signal_f<T> _send_signal_handler;
+    receive_signal_f<T> _receive_signal_handler;
 };
 }
--- processing_tests/processing_mac_tests/yas_processing_signal_generator_module_tests.mm
+++ processing_tests/processing_mac_tests/yas_processing_signal_generator_module_tests.mm
@@ -24,7 +24,7 @@ using namespace yas::processing;
 }
 
 - (void)test_create {
-    auto module = processing::make_signal_generator_module<double>(time_range{}, nullptr);
+    auto module = processing::make_signal_generator_module<double>({});
 
     XCTAssertTrue(module);
 }
@@ -35,30 +35,37 @@ using namespace yas::processing;
     XCTAssertFalse(module);
 }
 
-- (void)test_process_signal {
+- (void)test_process_send_signal {
     auto const ch_idx = 5;
+    auto const output_connector_key = "output";
 
     time_range process_time_range{.length = 2};
     stream stream = nullptr;
     time_range called_time_range;
-    double called_signal[2];
+    std::string called_key;
+    int64_t called_ch_idx;
 
-    auto clear = [&called_time_range, &called_signal]() {
+    auto clear = [&called_time_range, &called_key, &called_ch_idx]() {
         called_time_range.start_frame = 0;
         called_time_range.length = 0;
-        called_signal[0] = 0.0;
-        called_signal[1] = 0.0;
+        called_key.clear();
+        called_ch_idx = 0;
     };
 
-    auto handler = [&called_time_range, &called_signal](processing::time_range const &time_range, int64_t const ch_idx, double *signal_ptr) {
+    auto handler = [&called_time_range, &called_key, &called_ch_idx](processing::time_range const &time_range,
+                                                                     int64_t const ch_idx, std::string const &key,
+                                                                     double *const signal_ptr) {
         called_time_range = time_range;
+        called_key = key;
+        called_ch_idx = ch_idx;
         for (auto const &idx : make_each(time_range.length)) {
             signal_ptr[idx] = 1.0;
         }
     };
 
-    auto module = make_signal_generator_module<double>({.start_frame = 1, .length = 4}, std::move(handler));
-    module.connect_output(signal_generator_module::output_key, ch_idx);
+    auto module = make_signal_generator_module<double>(
+        {.time_range = {.start_frame = 1, .length = 4}, .send_signal_handler = std::move(handler)});
+    module.connect_output(output_connector_key, ch_idx);
 
     {
         clear();
@@ -70,6 +77,8 @@ using namespace yas::processing;
 
         XCTAssertEqual(called_time_range.start_frame, 1);
         XCTAssertEqual(called_time_range.length, 1);
+        XCTAssertEqual(called_key, output_connector_key);
+        XCTAssertEqual(called_ch_idx, ch_idx);
 
         XCTAssertTrue(stream.has_channel(ch_idx));
         auto const &raw = get_raw<double>((*stream.channel(ch_idx).datas().begin()).second);
@@ -87,6 +96,8 @@ using namespace yas::processing;
 
         XCTAssertEqual(called_time_range.start_frame, 2);
         XCTAssertEqual(called_time_range.length, 2);
+        XCTAssertEqual(called_key, output_connector_key);
+        XCTAssertEqual(called_ch_idx, ch_idx);
 
         XCTAssertTrue(stream.has_channel(ch_idx));
         auto const &raw = get_raw<double>((*stream.channel(ch_idx).datas().begin()).second);
@@ -105,6 +116,8 @@ using namespace yas::processing;
 
         XCTAssertEqual(called_time_range.start_frame, 4);
         XCTAssertEqual(called_time_range.length, 1);
+        XCTAssertEqual(called_key, output_connector_key);
+        XCTAssertEqual(called_ch_idx, ch_idx);
 
         XCTAssertTrue(stream.has_channel(ch_idx));
         auto const &raw = get_raw<double>((*stream.channel(ch_idx).datas().begin()).second);
@@ -121,8 +134,128 @@ using namespace yas::processing;
         module.process(stream);
 
         XCTAssertEqual(called_time_range.length, 0);
+        XCTAssertTrue(called_key.empty());
+        XCTAssertEqual(called_ch_idx, 0);
 
         XCTAssertFalse(stream.has_channel(ch_idx));
+    }
+}
+
+- (void)test_process_receive_signal {
+    auto const ch_idx = 7;
+    auto const input_connector_key = "input";
+
+    time_range process_time_range{.length = 2};
+    stream stream = nullptr;
+    time_range called_time_range;
+    std::string called_key;
+    int64_t called_ch_idx;
+    double called_signal[2];
+
+    auto stream_data = processing::make_data<double>(2);
+    auto &straem_data_raw = get_raw<double>(stream_data);
+    straem_data_raw[0] = 1.0;
+    straem_data_raw[1] = 1.0;
+
+    auto clear = [&called_time_range, &called_key, &called_ch_idx, &called_signal]() {
+        called_time_range.start_frame = 0;
+        called_time_range.length = 0;
+        called_key.clear();
+        called_ch_idx = 0;
+        called_signal[0] = 0.0;
+        called_signal[1] = 0.0;
+    };
+
+    auto make_stream = [&straem_data_raw, &ch_idx](time_range const &time_range) {
+        auto stream = processing::stream{time_range};
+        stream.insert_channel(ch_idx);
+
+        auto &channel = stream.channel(ch_idx);
+        channel.insert_data(time_range, straem_data_raw);
+
+        return stream;
+    };
+
+    auto handler = [&called_time_range, &called_key, &called_ch_idx, &called_signal](
+        processing::time_range const &time_range, int64_t const ch_idx, std::string const &key,
+        double const *const signal_ptr) {
+        called_time_range = time_range;
+        called_key = key;
+        called_ch_idx = ch_idx;
+        for (auto const &idx : make_each(time_range.length)) {
+            called_signal[idx] = signal_ptr[idx];
+        }
+    };
+
+    auto module = make_signal_generator_module<double>(
+        {.time_range = {.start_frame = 1, .length = 4}, .receive_signal_handler = std::move(handler)});
+    module.connect_input(input_connector_key, ch_idx);
+
+    {
+        clear();
+
+        process_time_range.start_frame = 0;
+        stream = make_stream(process_time_range);
+
+        module.process(stream);
+
+        XCTAssertEqual(called_time_range.start_frame, 1);
+        XCTAssertEqual(called_time_range.length, 1);
+        XCTAssertEqual(called_key, input_connector_key);
+        XCTAssertEqual(called_ch_idx, ch_idx);
+
+        XCTAssertEqual(called_signal[0], 1.0);
+        XCTAssertEqual(called_signal[1], 0.0);
+    }
+
+    {
+        clear();
+
+        process_time_range.start_frame = 2;
+        stream = make_stream(process_time_range);
+
+        module.process(stream);
+
+        XCTAssertEqual(called_time_range.start_frame, 2);
+        XCTAssertEqual(called_time_range.length, 2);
+        XCTAssertEqual(called_key, input_connector_key);
+        XCTAssertEqual(called_ch_idx, ch_idx);
+
+        XCTAssertEqual(called_signal[0], 1.0);
+        XCTAssertEqual(called_signal[1], 1.0);
+    }
+
+    {
+        clear();
+
+        process_time_range.start_frame = 4;
+        stream = make_stream(process_time_range);
+
+        module.process(stream);
+
+        XCTAssertEqual(called_time_range.start_frame, 4);
+        XCTAssertEqual(called_time_range.length, 1);
+        XCTAssertEqual(called_key, input_connector_key);
+        XCTAssertEqual(called_ch_idx, ch_idx);
+
+        XCTAssertEqual(called_signal[0], 1.0);
+        XCTAssertEqual(called_signal[1], 0.0);
+    }
+
+    {
+        clear();
+
+        process_time_range.start_frame = 6;
+        stream = make_stream(process_time_range);
+
+        module.process(stream);
+
+        XCTAssertEqual(called_time_range.length, 0);
+        XCTAssertTrue(called_key.empty());
+        XCTAssertEqual(called_ch_idx, 0);
+
+        XCTAssertEqual(called_signal[0], 0.0);
+        XCTAssertEqual(called_signal[1], 0.0);
     }
 }
 
