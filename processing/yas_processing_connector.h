413f38f3880886fc613353ff37e970bee631b4ca
Yuki Yasoshima <yukiyasos@gmail.com>
2017-01-07 10:53:22 +0900

    udpate connector

--- processing/yas_processing_connector.h
+++ processing/yas_processing_connector.h
@@ -7,14 +7,14 @@
 #include "yas_processing_types.h"
 #include <string>
 #include <experimental/optional>
-#include <unordered_map>
+#include <map>
 
 namespace yas {
 namespace processing {
     struct connector {
         channel_index_t channel_index;
     };
-    
-    using connector_map_t = std::unordered_map<std::string, connector>;
+
+    using connector_map_t = std::map<std::string, connector>;
 }
 }
