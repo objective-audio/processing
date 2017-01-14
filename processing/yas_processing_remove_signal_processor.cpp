b8facb5ad38faa20eca929650b99e553f7a6b19f
Yuki Yasoshima <yukiyasos@gmail.com>
2017-01-14 18:05:04 +0900

    add remove_signal_processor

---
+++ processing/yas_processing_remove_signal_processor.cpp
@@ -0,0 +1,63 @@
+//
+//  yas_processing_remove_signal_processor.cpp
+//
+
+#include "yas_processing_remove_signal_processor.h"
+#include "yas_processing_stream.h"
+#include "yas_processing_channel.h"
+
+using namespace yas;
+
+template <typename T>
+processing::processor_f processing::make_remove_signal_processor() {
+    return [](time::range const &current_time_range, connector_map_t const &input_connectors, connector_map_t const &,
+              stream &stream) {
+        for (auto const &connector_pair : input_connectors) {
+            auto const &connector = connector_pair.second;
+
+            auto const &ch_idx = connector.channel_index;
+
+            if (stream.has_channel(ch_idx)) {
+                auto &channel = stream.channel(ch_idx);
+
+                auto predicate = [&current_time_range](auto const &pair) {
+                    time::range const &event_time_range = pair.first;
+                    return event_time_range.is_overlap(current_time_range);
+                };
+
+                auto const filtered_events = channel.filtered_events<T, signal_event>(predicate);
+
+                std::vector<std::pair<time::range, signal_event>> cropped_signals;
+
+                for (auto const &event_pair : filtered_events) {
+                    auto const &src_frame = event_pair.first.frame;
+                    auto const cropped_ranges = event_pair.first.crop(current_time_range);
+                    signal_event const &src_signal = event_pair.second;
+                    for (auto const &cropped_range : cropped_ranges) {
+                        signal_event dst_signal = make_signal_event<T>(cropped_range.length);
+                        auto const *src_ptr = &src_signal.data<T>()[cropped_range.frame - src_frame];
+                        dst_signal.copy_from(src_ptr, cropped_range.length);
+                        cropped_signals.emplace_back(std::make_pair(cropped_range, std::move(dst_signal)));
+                    }
+                }
+
+                channel.erase_event<T, signal_event>(std::move(predicate));
+
+                for (auto const &pair : cropped_signals) {
+                    channel.insert_event(time{pair.first}, pair.second);
+                }
+            }
+        }
+    };
+}
+
+template processing::processor_f processing::make_remove_signal_processor<double>();
+template processing::processor_f processing::make_remove_signal_processor<float>();
+template processing::processor_f processing::make_remove_signal_processor<int64_t>();
+template processing::processor_f processing::make_remove_signal_processor<int32_t>();
+template processing::processor_f processing::make_remove_signal_processor<int16_t>();
+template processing::processor_f processing::make_remove_signal_processor<int8_t>();
+template processing::processor_f processing::make_remove_signal_processor<uint64_t>();
+template processing::processor_f processing::make_remove_signal_processor<uint32_t>();
+template processing::processor_f processing::make_remove_signal_processor<uint16_t>();
+template processing::processor_f processing::make_remove_signal_processor<uint8_t>();
---
+++ processing/yas_processing_remove_signal_processor.h
@@ -0,0 +1,14 @@
+//
+//  yas_processing_remove_signal_processor.h
+//
+
+#pragma once
+
+#include "yas_processing_processor.h"
+
+namespace yas {
+namespace processing {
+    template <typename T>
+    processor_f make_remove_signal_processor();
+}
+}
