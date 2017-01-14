a644ba7f7a710a7b77dbdb9a04221cfa4b79ae56
Yuki Yasoshima <yukiyasos@gmail.com>
2017-01-14 13:06:49 +0900

    name erase_event

--- processing/yas_processing_channel.h
+++ processing/yas_processing_channel.h
@@ -35,7 +35,7 @@ namespace processing {
         template <typename T, typename Event>
         void erase_event();
         template <typename T, typename Event, typename P>
-        void erase_event_if(P predicate);
+        void erase_event(P predicate);
     };
 }
 }
--- processing/yas_processing_channel_private.h
+++ processing/yas_processing_channel_private.h
@@ -20,11 +20,13 @@ void processing::channel::erase_event() {
 }
 
 template <typename T, typename Event, typename P>
-void processing::channel::erase_event_if(P predicate) {
+void processing::channel::erase_event(P predicate) {
     erase_if(this->events(), [predicate = std::move(predicate)](auto const &pair) {
-        if (pair.first.type() == typeid(typename Event::time_type)) {
+        time const &time = pair.first;
+        if (time.type() == typeid(typename Event::time_type)) {
             if (auto const casted_event = yas::cast<Event>(pair.second)) {
-                if (casted_event.sample_type() == typeid(T) && predicate(pair)) {
+                if (casted_event.sample_type() == typeid(T) &&
+                    predicate(std::make_pair(time.get<typename Event::time_type>(), casted_event))) {
                     return true;
                 }
             }
@@ -42,11 +44,12 @@ template <typename T, typename Event, typename P>
 std::multimap<typename Event::time_type::type, Event> processing::channel::filtered_events(P predicate) const {
     std::multimap<typename Event::time_type::type, Event> filtered;
 
-    for (auto const &pair : events()) {
-        if (pair.first.type() == typeid(typename Event::time_type)) {
-            if (auto const casted_event = yas::cast<Event>(pair.second)) {
+    for (auto const &event_pair : events()) {
+        if (event_pair.first.type() == typeid(typename Event::time_type)) {
+            if (auto const casted_event = yas::cast<Event>(event_pair.second)) {
+                auto pair = std::make_pair(event_pair.first.get<typename Event::time_type>(), casted_event);
                 if (casted_event.sample_type() == typeid(T) && predicate(pair)) {
-                    filtered.insert(filtered.end(), {pair.first.get<typename Event::time_type>(), casted_event});
+                    filtered.insert(filtered.end(), std::move(pair));
                 }
             }
         }
