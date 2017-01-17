7a4e3a04354f00129ac66d25e9b59aae2e5c3871
Yuki Yasoshima <yukiyasos@gmail.com>
2017-01-17 21:01:14 +0900

    add zero_length

--- processing/yas_processing_types.h
+++ processing/yas_processing_types.h
@@ -16,6 +16,8 @@ namespace processing {
     using frame_index_t = int64_t;
     using length_t = uint32_t;
 
+    static length_t constexpr zero_length = 0;
+
     using connector_index_t = uint32_t;
     using connector_index_set_t = std::unordered_set<connector_index_t>;
 }
