6efffed77f4b0e7c08d3c25d6a1efad3dceeedfc
Yuki Yasoshima <yukiyasos@gmail.com>
2017-02-04 12:57:12 +0900

    constant boolean

--- processing/yas_processing_constant_module.cpp
+++ processing/yas_processing_constant_module.cpp
@@ -6,6 +6,7 @@
 #include "yas_processing_send_signal_processor.h"
 #include "yas_processing_module.h"
 #include "yas_fast_each.h"
+#include "yas_boolean.h"
 
 using namespace yas;
 
@@ -24,13 +25,14 @@ processing::module processing::constant::make_signal_module(T value) {
     return processing::module{{std::move(processor)}};
 }
 
-template processing::module processing::constant::make_signal_module<double>(double);
-template processing::module processing::constant::make_signal_module<float>(float);
-template processing::module processing::constant::make_signal_module<int64_t>(int64_t);
-template processing::module processing::constant::make_signal_module<int32_t>(int32_t);
-template processing::module processing::constant::make_signal_module<int16_t>(int16_t);
-template processing::module processing::constant::make_signal_module<int8_t>(int8_t);
-template processing::module processing::constant::make_signal_module<uint64_t>(uint64_t);
-template processing::module processing::constant::make_signal_module<uint32_t>(uint32_t);
-template processing::module processing::constant::make_signal_module<uint16_t>(uint16_t);
-template processing::module processing::constant::make_signal_module<uint8_t>(uint8_t);
+template processing::module processing::constant::make_signal_module(double);
+template processing::module processing::constant::make_signal_module(float);
+template processing::module processing::constant::make_signal_module(int64_t);
+template processing::module processing::constant::make_signal_module(int32_t);
+template processing::module processing::constant::make_signal_module(int16_t);
+template processing::module processing::constant::make_signal_module(int8_t);
+template processing::module processing::constant::make_signal_module(uint64_t);
+template processing::module processing::constant::make_signal_module(uint32_t);
+template processing::module processing::constant::make_signal_module(uint16_t);
+template processing::module processing::constant::make_signal_module(uint8_t);
+template processing::module processing::constant::make_signal_module(boolean);
