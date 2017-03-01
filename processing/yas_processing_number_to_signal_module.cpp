9e9250a826bf21d723e829c9064fec9c94c35640
Yuki Yasoshima <yukiyasos@gmail.com>
2017-03-01 23:07:32 +0900

    replace number_to_signal context

--- processing/yas_processing_number_to_signal_module.cpp
+++ processing/yas_processing_number_to_signal_module.cpp
@@ -9,52 +9,23 @@
 #include "yas_processing_receive_number_processor.h"
 #include "yas_processing_send_signal_processor.h"
 #include "yas_processing_remove_number_processor.h"
+#include "yas_processing_number_process_context.h"
 #include "yas_fast_each.h"
 #include "yas_boolean.h"
 #include <map>
 
 using namespace yas;
 
-namespace yas {
-namespace processing {
-    namespace number_to_signal {
-        template <typename T>
-        struct context {
-            std::map<time::frame::type, T> number_events;
-            T last_value = 0;
-
-            void reset() {
-                this->number_events.clear();
-            }
-        };
-
-        template <typename T>
-        using context_sptr = std::shared_ptr<context<T>>;
-
-        template <typename T>
-        context_sptr<T> make_context() {
-            return std::make_shared<context<T>>();
-        }
-    }
-}
-}
-
 template <typename T>
 processing::module processing::make_number_to_signal_module() {
-    auto context = number_to_signal::make_context<T>();
+    auto context = std::make_shared<number_process_context<T, 1>>();
 
-    auto prepare_processor = [context](time::range const &, connector_map_t const &, connector_map_t const &,
-                                       stream &) mutable { context->reset(); };
+    auto prepare_processor = [context](time::range const &current_range, connector_map_t const &,
+                                       connector_map_t const &, stream &) mutable { context->reset(current_range); };
 
-    auto receive_processor =
-        make_receive_number_processor<T>([context](processing::time::frame::type const &frame, channel_index_t const,
-                                                   connector_index_t const, T const &value) mutable {
-            auto &number_events = context->number_events;
-            if (number_events.count(frame) > 0) {
-                number_events.erase(frame);
-            }
-            context->number_events.emplace(std::make_pair(frame, value));
-        });
+    auto receive_processor = make_receive_number_processor<T>(
+        [context](processing::time::frame::type const &frame, channel_index_t const, connector_index_t const,
+                  T const &value) mutable { context->insert_input(frame, value, 0); });
 
     auto remove_processor = make_remove_number_processor<T>({to_connector_index(number_to_signal::input::number)});
 
@@ -62,16 +33,16 @@ processing::module processing::make_number_to_signal_module() {
         make_send_signal_processor<T>([context](processing::time::range const &time_range, sync_source const &,
                                                 channel_index_t const, connector_index_t const, T *const signal_ptr) {
             auto const top_frame = time_range.frame;
-            auto iterator = context->number_events.begin();
-            auto const end_iterator = context->number_events.end();
-            auto &last_value = context->last_value;
+            auto iterator = context->inputs().begin();
+            auto const end_iterator = context->inputs().end();
+            T const &last_value = context->last_values()[0];
 
             auto out_each = make_fast_each(signal_ptr, time_range.length);
             while (yas_fast_each_next(out_each)) {
                 auto const frame = top_frame + yas_fast_each_index(out_each);
                 if (iterator != end_iterator) {
                     if (iterator->first == frame) {
-                        last_value = iterator->second;
+                        context->update_last_values(iterator->second);
                         ++iterator;
                     }
                 }
