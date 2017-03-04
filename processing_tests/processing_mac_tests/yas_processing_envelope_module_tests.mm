76051a8942cd1b1bacb5262e1d27fd01ec577a07
Yuki Yasoshima <yukiyasos@gmail.com>
2017-03-04 19:00:33 +0900

    add envelope offset test

--- processing_tests/processing_mac_tests/yas_processing_envelope_module_tests.mm
+++ processing_tests/processing_mac_tests/yas_processing_envelope_module_tests.mm
@@ -88,4 +88,39 @@ using namespace yas::processing;
     XCTAssertEqual(data[0], 0);
 }
 
+- (void)test_process_offset {
+    length_t const process_length = 5;
+    channel_index_t const ch_idx = 10;
+    frame_index_t const offset = 100;
+    
+    stream stream{sync_source{1, process_length}};
+    
+    envelope::anchors_t<int8_t> anchors;
+    anchors.emplace(1, 2);
+    anchors.emplace(3, 4);
+    
+    auto module = envelope::make_signal_module(std::move(anchors), offset);
+    connect(module, envelope::output::value, ch_idx);
+    
+    module.process(time::range{offset, process_length}, stream);
+    
+    XCTAssertTrue(stream.has_channel(ch_idx));
+    
+    auto const &channel = stream.channel(ch_idx);
+    
+    auto const &time = channel.events().cbegin()->first;
+    auto const &time_range = time.get<time::range>();
+    
+    XCTAssertEqual(time_range, time::range(offset, process_length));
+    
+    auto const &signal = cast<signal_event>(channel.events().cbegin()->second);
+    auto *data = signal.data<int8_t>();
+    
+    XCTAssertEqual(data[0], 2);
+    XCTAssertEqual(data[1], 2);
+    XCTAssertEqual(data[2], 3);
+    XCTAssertEqual(data[3], 4);
+    XCTAssertEqual(data[4], 4);
+}
+
 @end
