9097b96e4ab310202a1e5a63dfb4712246e43406
Yuki Yasoshima <yukiyasos@gmail.com>
2018-02-15 23:51:37 +0900

    event namespace

--- processing/event/yas_processing_number_event.h
+++ processing/event/yas_processing_number_event.h
@@ -7,35 +7,33 @@
 #include "yas_processing_event.h"
 #include "yas_processing_time.h"
 
-namespace yas {
-namespace processing {
-    class number_event : public event {
-       public:
-        class impl;
-
-        template <typename T>
-        class type_impl;
-
-        using time_type = time::frame;
-        
-        template <typename T>
-        using value_map_t = std::multimap<time::frame::type, T>;
-        
-        template <typename T>
-        explicit number_event(T);
-
-        number_event(std::nullptr_t);
-
-        std::type_info const &sample_type() const;
-        std::size_t sample_byte_count() const;
-
-        template <typename T>
-        T const &get() const;
-    };
+namespace yas::processing {
+class number_event : public event {
+   public:
+    class impl;
 
     template <typename T>
-    number_event make_number_event(T);
-}
+    class type_impl;
+
+    using time_type = time::frame;
+
+    template <typename T>
+    using value_map_t = std::multimap<time::frame::type, T>;
+
+    template <typename T>
+    explicit number_event(T);
+
+    number_event(std::nullptr_t);
+
+    std::type_info const &sample_type() const;
+    std::size_t sample_byte_count() const;
+
+    template <typename T>
+    T const &get() const;
+};
+
+template <typename T>
+number_event make_number_event(T);
 }
 
 #include "yas_processing_number_event_private.h"
