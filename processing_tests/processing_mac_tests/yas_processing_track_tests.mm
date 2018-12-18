3faff87a1547f19eacbac7fc5c2b73aa4d4160c1
Yuki Yasoshima <yukiyasos@gmail.com>
2018-12-18 18:06:12 +0900

    fix test

--- processing_tests/processing_mac_tests/yas_processing_track_tests.mm
+++ processing_tests/processing_mac_tests/yas_processing_track_tests.mm
@@ -69,25 +69,21 @@ using namespace yas;
 - (void)test_total_range {
     proc::track track;
 
-    proc::module module1{proc::module::processors_t{}};
-    proc::module module2{proc::module::processors_t{}};
-    proc::module module3{proc::module::processors_t{}};
-
     XCTAssertFalse(track.total_range());
 
-    track.insert_module({0, 1}, std::move(module1));
+    track.insert_module({0, 1}, proc::module{proc::module::processors_t{}});
 
     XCTAssertEqual(track.total_range(), (proc::time::range{0, 1}));
 
-    track.insert_module({1, 1}, std::move(module2));
+    track.insert_module({1, 1}, proc::module{proc::module::processors_t{}});
 
     XCTAssertEqual(track.total_range(), (proc::time::range{0, 2}));
 
-    track.insert_module({99, 1}, std::move(module3));
+    track.insert_module({99, 1}, proc::module{proc::module::processors_t{}});
 
     XCTAssertEqual(track.total_range(), (proc::time::range{0, 100}));
 
-    track.insert_module({-10, 1}, std::move(module3));
+    track.insert_module({-10, 1}, proc::module{proc::module::processors_t{}});
 
     XCTAssertEqual(track.total_range(), (proc::time::range{-10, 110}));
 }
