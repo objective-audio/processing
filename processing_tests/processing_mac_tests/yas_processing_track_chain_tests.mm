a7e6e7490b80631b791e4b220c0782f37a6dd041
Yuki Yasoshima <yukiyasos@gmail.com>
2018-12-29 10:58:43 +0900

    add track_chain_tests file

---
+++ processing_tests/processing_mac_tests/yas_processing_track_chain_tests.mm
@@ -0,0 +1,22 @@
+//
+//  yas_processing_track_chain_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_processing.h"
+
+using namespace yas;
+
+@interface yas_processing_track_chain_tests : XCTestCase
+
+@end
+
+@implementation yas_processing_track_chain_tests
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+@end
