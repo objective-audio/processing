a774cb3d51c68bb315c4124dcda2722c60ff88ab
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-07 22:28:55 +0900

    processing_ios

---
+++ processing_ios/processing/processing.h
@@ -0,0 +1,13 @@
+//
+//  processing.h
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for processing.
+FOUNDATION_EXPORT double processingVersionNumber;
+
+//! Project version string for processing.
+FOUNDATION_EXPORT const unsigned char processingVersionString[];
+
+#import <processing/yas_processing_umbrella.h>
