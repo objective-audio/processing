7da08a141ebd80e7c97ef388426d446e6721e80c
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-19 13:16:38 +0900

    timeline fetched test

--- processing_tests/yas_processing_timeline_chain_tests.mm
+++ processing_tests/yas_processing_timeline_chain_tests.mm
@@ -21,6 +21,26 @@ using namespace yas::proc;
 }
 
 - (void)test_fetched {
+    timeline timeline;
+
+    track track0;
+    track track1;
+
+    timeline.insert_track(0, track0);
+    timeline.insert_track(1, track1);
+
+    std::vector<timeline::event_t> events;
+
+    auto chain = timeline.chain().perform([&events](auto const &event) { events.push_back(event); }).sync();
+
+    XCTAssertEqual(events.size(), 1);
+    XCTAssertEqual(events.at(0).type(), chaining::event_type::fetched);
+    auto iterator = events.at(0).get<timeline::fetched_event_t>().elements.begin();
+    XCTAssertEqual(iterator->first, 0);
+    XCTAssertEqual(iterator->second, track0);
+    ++iterator;
+    XCTAssertEqual(iterator->first, 1);
+    XCTAssertEqual(iterator->second, track1);
 }
 
 - (void)test_inserted {
