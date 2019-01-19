7b0a59630101b70900c60c6fc27a898e3fb11338
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-19 12:54:04 +0900

    add timeline chain tests file

---
+++ processing_tests/yas_processing_timeline_chain_tests.mm
@@ -0,0 +1,39 @@
+//
+//  yas_processing_timeline_chain_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import <processing/yas_processing_umbrella.h>
+
+using namespace yas;
+using namespace yas::proc;
+
+@interface yas_processing_timeline_chain_tests : XCTestCase
+
+@end
+
+@implementation yas_processing_timeline_chain_tests
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+- (void)test_fetched {
+}
+
+- (void)test_inserted {
+}
+
+- (void)test_erased {
+}
+
+- (void)test_replaced {
+    // ???
+}
+
+- (void)test_relayed {
+}
+
+@end
