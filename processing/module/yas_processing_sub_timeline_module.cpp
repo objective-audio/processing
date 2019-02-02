9a5ed61ca812def1bcc64e4c2a8b6cb0ed0ed9d2
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-02 11:11:19 +0900

    update sub_timeline_module

--- processing/module/yas_processing_sub_timeline_module.cpp
+++ processing/module/yas_processing_sub_timeline_module.cpp
@@ -28,50 +28,54 @@ namespace sub_timeline {
 proc::module proc::make_module(timeline timeline, frame_index_t const offset) {
     auto context = sub_timeline::make_context(std::move(timeline));
 
-    auto processor = [context, offset](time::range const &time_range, connector_map_t const &input_connectors,
-                                       connector_map_t const &output_connectors, stream &stream) mutable {
-        proc::stream sub_stream{stream.sync_source()};
+    auto make_processors = [context = std::move(context), offset] {
+        auto processor = [context, offset](time::range const &time_range, connector_map_t const &input_connectors,
+                                           connector_map_t const &output_connectors, stream &stream) mutable {
+            proc::stream sub_stream{stream.sync_source()};
 
-        for (auto const &connector : input_connectors) {
-            auto const &ch_idx = connector.second.channel_index;
+            for (auto const &connector : input_connectors) {
+                auto const &ch_idx = connector.second.channel_index;
 
-            if (stream.has_channel(ch_idx)) {
-                auto const &input_channel = stream.channel(ch_idx);
+                if (stream.has_channel(ch_idx)) {
+                    auto const &input_channel = stream.channel(ch_idx);
+                    auto const &co_idx = connector.first;
+
+                    if (sub_stream.has_channel(co_idx)) {
+                        throw "channel already exists in sub_stream.";
+                    }
+
+                    sub_stream.add_channel(co_idx, input_channel.copied_events(time_range, -offset));
+                }
+            }
+
+            context->timeline.process(time_range.offset(-offset), sub_stream);
+
+            for (auto const &connector : output_connectors) {
                 auto const &co_idx = connector.first;
 
                 if (sub_stream.has_channel(co_idx)) {
-                    throw "channel already exists in sub_stream.";
-                }
+                    auto const &ch_idx = connector.second.channel_index;
+                    auto &out_channel = stream.add_channel(ch_idx);
+                    auto const &sub_channel = sub_stream.channel(co_idx);
 
-                sub_stream.add_channel(co_idx, input_channel.copied_events(time_range, -offset));
-            }
-        }
+                    out_channel.erase_events(time_range);
 
-        context->timeline.process(time_range.offset(-offset), sub_stream);
-
-        for (auto const &connector : output_connectors) {
-            auto const &co_idx = connector.first;
-
-            if (sub_stream.has_channel(co_idx)) {
-                auto const &ch_idx = connector.second.channel_index;
-                auto &out_channel = stream.add_channel(ch_idx);
-                auto const &sub_channel = sub_stream.channel(co_idx);
-
-                out_channel.erase_events(time_range);
-
-                for (auto &event_pair : sub_channel.events()) {
-                    auto const &time = event_pair.first;
-                    auto const &event = event_pair.second;
-                    if (time.is_range_type()) {
-                        out_channel.combine_signal_event(time.get<time::range>().offset(offset),
-                                                         cast<signal_event>(event));
-                    } else {
-                        out_channel.insert_event(time.offset(offset), event);
+                    for (auto &event_pair : sub_channel.events()) {
+                        auto const &time = event_pair.first;
+                        auto const &event = event_pair.second;
+                        if (time.is_range_type()) {
+                            out_channel.combine_signal_event(time.get<time::range>().offset(offset),
+                                                             cast<signal_event>(event));
+                        } else {
+                            out_channel.insert_event(time.offset(offset), event);
+                        }
                     }
                 }
             }
-        }
+        };
+
+        return module::processors_t{std::move(processor)};
     };
 
-    return proc::module{{std::move(processor)}};
+    return proc::module{std::move(make_processors)};
 }
