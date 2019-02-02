e4e089368cb25dc2e331ff414522a154cc8d96df
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-02 09:47:59 +0900

    update compare_module

--- processing/module/yas_processing_compare_modules.cpp
+++ processing/module/yas_processing_compare_modules.cpp
@@ -23,71 +23,79 @@ template <typename T>
 proc::module proc::make_signal_module(compare::kind const kind) {
     using namespace yas::proc::compare;
 
-    auto context = std::make_shared<signal_process_context<T, 2>>();
+    auto make_processors = [kind] {
+        auto context = std::make_shared<signal_process_context<T, 2>>();
 
-    auto prepare_processor = [context](time::range const &, connector_map_t const &, connector_map_t const &,
-                                       stream &stream) mutable { context->reset(stream.sync_source().slice_length); };
+        auto prepare_processor = [context](time::range const &, connector_map_t const &, connector_map_t const &,
+                                           stream &stream) mutable {
+            context->reset(stream.sync_source().slice_length);
+        };
 
-    auto receive_processor = proc::make_receive_signal_processor<T>(
-        [context](time::range const &time_range, sync_source const &, channel_index_t const,
-                  connector_index_t const co_idx, T const *const signal_ptr) mutable {
-            if (co_idx == to_connector_index(input::left) || co_idx == to_connector_index(input::right)) {
-                context->set_time(time{time_range}, co_idx);
-                context->copy_data_from(signal_ptr, time_range.length, co_idx);
-            }
-        });
+        auto receive_processor = proc::make_receive_signal_processor<T>(
+            [context](time::range const &time_range, sync_source const &, channel_index_t const,
+                      connector_index_t const co_idx, T const *const signal_ptr) mutable {
+                if (co_idx == to_connector_index(input::left) || co_idx == to_connector_index(input::right)) {
+                    context->set_time(time{time_range}, co_idx);
+                    context->copy_data_from(signal_ptr, time_range.length, co_idx);
+                }
+            });
 
-    auto send_processor = proc::make_send_signal_processor<boolean>(
-        [context, kind, out_each = fast_each<boolean *>{}](proc::time::range const &time_range, sync_source const &,
-                                                           channel_index_t const, connector_index_t const co_idx,
-                                                           boolean *const signal_ptr) mutable {
-            if (co_idx == to_connector_index(output::result)) {
-                static auto const left_co_idx = to_connector_index(input::left);
-                static auto const right_co_idx = to_connector_index(input::right);
+        auto send_processor = proc::make_send_signal_processor<boolean>(
+            [context, kind, out_each = fast_each<boolean *>{}](proc::time::range const &time_range, sync_source const &,
+                                                               channel_index_t const, connector_index_t const co_idx,
+                                                               boolean *const signal_ptr) mutable {
+                if (co_idx == to_connector_index(output::result)) {
+                    static auto const left_co_idx = to_connector_index(input::left);
+                    static auto const right_co_idx = to_connector_index(input::right);
 
-                auto const *left_ptr = context->data(left_co_idx);
-                auto const *right_ptr = context->data(right_co_idx);
-                proc::time const &left_time = context->time(left_co_idx);
-                proc::time const &right_time = context->time(right_co_idx);
-                auto const left_offset = left_time ? time_range.frame - left_time.get<time::range>().frame : 0;
-                auto const right_offset = right_time ? time_range.frame - right_time.get<time::range>().frame : 0;
-                auto const &left_length = left_time ? left_time.get<time::range>().length : constant::zero_length;
-                auto const &right_length = right_time ? right_time.get<time::range>().length : constant::zero_length;
+                    auto const *left_ptr = context->data(left_co_idx);
+                    auto const *right_ptr = context->data(right_co_idx);
+                    proc::time const &left_time = context->time(left_co_idx);
+                    proc::time const &right_time = context->time(right_co_idx);
+                    auto const left_offset = left_time ? time_range.frame - left_time.get<time::range>().frame : 0;
+                    auto const right_offset = right_time ? time_range.frame - right_time.get<time::range>().frame : 0;
+                    auto const &left_length = left_time ? left_time.get<time::range>().length : constant::zero_length;
+                    auto const &right_length =
+                        right_time ? right_time.get<time::range>().length : constant::zero_length;
 
-                out_each.reset(signal_ptr, time_range.length);
-                while (yas_each_next(out_each)) {
-                    auto const &idx = yas_each_index(out_each);
-                    auto const left_idx = idx + left_offset;
-                    auto const right_idx = idx + right_offset;
-                    auto const &left_value = (left_idx >= 0 && left_idx < left_length) ? left_ptr[left_idx] : 0;
-                    auto const &right_value = (right_idx >= 0 && right_idx < right_length) ? right_ptr[right_idx] : 0;
+                    out_each.reset(signal_ptr, time_range.length);
+                    while (yas_each_next(out_each)) {
+                        auto const &idx = yas_each_index(out_each);
+                        auto const left_idx = idx + left_offset;
+                        auto const right_idx = idx + right_offset;
+                        auto const &left_value = (left_idx >= 0 && left_idx < left_length) ? left_ptr[left_idx] : 0;
+                        auto const &right_value =
+                            (right_idx >= 0 && right_idx < right_length) ? right_ptr[right_idx] : 0;
 
-                    switch (kind) {
-                        case kind::is_equal:
-                            yas_each_value(out_each) = left_value == right_value;
-                            break;
-                        case kind::is_not_equal:
-                            yas_each_value(out_each) = left_value != right_value;
-                            break;
+                        switch (kind) {
+                            case kind::is_equal:
+                                yas_each_value(out_each) = left_value == right_value;
+                                break;
+                            case kind::is_not_equal:
+                                yas_each_value(out_each) = left_value != right_value;
+                                break;
 
-                        case kind::is_greater:
-                            yas_each_value(out_each) = left_value > right_value;
-                            break;
-                        case kind::is_greater_equal:
-                            yas_each_value(out_each) = left_value >= right_value;
-                            break;
-                        case kind::is_less:
-                            yas_each_value(out_each) = left_value < right_value;
-                            break;
-                        case kind::is_less_equal:
-                            yas_each_value(out_each) = left_value <= right_value;
-                            break;
+                            case kind::is_greater:
+                                yas_each_value(out_each) = left_value > right_value;
+                                break;
+                            case kind::is_greater_equal:
+                                yas_each_value(out_each) = left_value >= right_value;
+                                break;
+                            case kind::is_less:
+                                yas_each_value(out_each) = left_value < right_value;
+                                break;
+                            case kind::is_less_equal:
+                                yas_each_value(out_each) = left_value <= right_value;
+                                break;
+                        }
                     }
                 }
-            }
-        });
+            });
+        return std::vector<proc::processor_f>{
+            {std::move(prepare_processor), std::move(receive_processor), std::move(send_processor)}};
+    };
 
-    return proc::module{{std::move(prepare_processor), std::move(receive_processor), std::move(send_processor)}};
+    return proc::module{std::move(make_processors)};
 }
 
 template proc::module proc::make_signal_module<double>(compare::kind const);
